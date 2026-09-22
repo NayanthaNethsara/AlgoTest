3f0d628611eea059738e63b8018223fe49aa2dbc
NayanthaNethsara <nayanthanethsara@gmail.com>
2026-09-23 01:11:56 +0530

    feat: update auth package name and add branding package with initial content

---
+++ packages/branding/src/index.ts
@@ -0,0 +1,8 @@
+export const PRODUCT_NAME = "Labyrithm";
+export const PRODUCT_TAGLINE =
+  "Self-hosted infrastructure for algorithmic problem solving.";
+export const PRODUCT_DESCRIPTION =
+  "Self-hosted platform for algorithmic problem solving and secure code evaluation";
+export const ADMIN_DESCRIPTION =
+  "Management console for Labyrithm problem-solving environments and assessments";
+export const DEFAULT_SESSION_TITLE = "Labyrithm Session";
